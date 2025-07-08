dbb119a19f35b8163dd168f8ee60d8bbb19e079f
AalindCS <kaleaalind7@gmail.com>
2025-07-08 12:53:21 -0700

    First Submission

---
+++ Day1.py
@@ -0,0 +1,30 @@
+# Day1
+# Print a statement
+'''print("Hello World")
+print("Hello World\nHello World") #For a new line: use \n
+
+#Concatenation of strings
+print("Hello" + " " + "Angela")
+
+# Input function
+input("What is your name?")
+print("Hello " + input("What is your name?") + "!")
+
+# Variables and length
+name = input("What is your name?")
+print(name)
+name = "Angela"
+print(name)
+print(len(name))
+print(len(input("What is your name?")))
+name = input("What is your name?")
+length = len(name)
+print(length)
+# There should be no spaces in variable naming
+# Can only be started with a letter or an underscore'''
+
+# Day1 project: Band Name Generator
+print("Welcome to the Band Name Generator")
+a = input("What's the name of the city you grew up in?\n")
+b = input("What's your pet's name?\n")
+print("Your band name could be "+ a + " " + b)
